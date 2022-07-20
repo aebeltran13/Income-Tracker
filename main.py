966d18b3ebf85a59a197e0cf83fb8d5cd386dbbf
Alfonso Beltran <aebeltran13@gmail.com>
2022-07-20 02:44:24 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Author: Alfonso Beltran
+# Start Data: 7/20/22
+# Topics: File reading, GUI, Automation
+
+# This application is a basic Income tracker with a basic GUI.
+# The purpose is to replicate what I do in a spread sheet and automate my monthly
+# income tracking.
+
+if __name__ == '__main__':
+
+    # Read file
+
+
+    # Welcome user
+
+
+    # Menu (Add new month, View precious months, Edit Prevous month)
+
+
+    # View Months
+
+    # Edit Month
+
+    # Add month
+
+
+
+
+
+    
+
+    print('Slay')
